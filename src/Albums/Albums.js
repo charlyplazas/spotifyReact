400291c57953707c0527fd28270ebdf13c5dce42
CHARLY <charlyplazas0621@gmail.com>
2022-10-20 13:19:52 -0500

    se pinta la musica

--- src/Albums/Albums.js
+++ src/Albums/Albums.js
@@ -1,71 +1,51 @@
-import { useState, useEffect } from "react"
-import { Musicos } from "../Musicos/Musicos.js"
-import { servicioSpoty } from "../services/servicioSpoty.js"
-import { servicioTOKEN } from "../services/servicioTOKEN.js"
+import { useState, useEffect } from "react";
+import { Musicos } from "../Musicos/Musicos.js";
+import { servicioSpoty } from "../services/servicioSpoty.js";
+import { servicioTOKEN } from "../services/servicioTOKEN.js";
 
+export function Albums() {
+  //declarando mi primer useState
 
-export function Albums(){
+  const [canciones, setCanciones] = useState(null);
 
-//declarando mi primer useState
+  //useState para la carga de datos
+  const [carga, setCarga] = useState(true);
 
-const [canciones,setCanciones]=useState(null)
+  //usando el useEffect
 
-//useState para la carga de datos
-const [carga,setCarga]=useState(true)
-
-//usando el useEffect
-
-useEffect(function(){
-    servicioSpoty()
-    .then(function(respuesta){
-        setCanciones(respuesta.tracks)
-        setCarga(false)
-    })    
+  useEffect(function () {
+    servicioSpoty().then(function (respuesta) {
+      setCanciones(respuesta.tracks);
+      setCarga(false);
+    });
     console.log(canciones);
-    
-},[])
+  }, []);
 
-if (carga==true) {
-    return(
-        <>
+  if (carga == true) {
+    return (
+      <>
         <h1>estoy cargando....</h1>
-        </>
-    )
-}else{
-    return(
-        <>
+      </>
+    );
+  } else {
+    return (
+      <>
         <div className="row row-cols-1 row-cols-md-4 g-3 justify-content-center mt-5">
-        {
-            canciones.map(function(cancion){ 
-        return(
-            <>
-            <div className="col mt-3 justify-content-center">
-            <div className="card h-100">
-              <audio src={cancion.preview_url} controls />
-              <h3 className="text-center">{cancion.name}</h3>
-              <img src={cancion.album.images[0].url} />
-             
-            </div>
-          </div>
-            </>
-        )
-        
-            })
-        }
-
-
-
+          {canciones.map(function (cancion) {
+            return (
+              <>
+                <div className="col mt-3 justify-content-center">
+                  <div className="card h-100">
+                    <audio src={cancion.preview_url} controls />
+                    <h3 className="text-center">{cancion.name}</h3>
+                    <img src={cancion.album.images[0].url} />
+                  </div>
+                </div>
+              </>
+            );
+          })}
         </div>
-        </>
-    )
+      </>
+    );
+  }
 }
-
-
-
-
-
-
-
-
-
-    }
